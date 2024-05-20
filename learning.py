21b8a4e2b9898af2190e09d7b6990be726f178e5
adanilishin <adanilishin@WIN-ARFHPLSLFR9>
2024-05-20 08:36:10 +0300

    some experiments

--- learning.py
+++ learning.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torch.utils.tensorboard import SummaryWriter
 
 
 class Learning:
@@ -16,23 +17,40 @@ class Learning:
         result = 0
         counter = 0
         for par in model.parameters():
-            result += torch.norm(par.data.clone() - parameters[counter])
+            result += torch.norm(par.data.clone().cpu() - parameters[counter].clone().cpu())
             counter += 1
         return result
 
-    def train(self, model, criterion, optimizer, writer, epochs=100):
+    def train(self, model, criterion, optimizer, writer, epochs=100, precision='fp16'):
+        scaler = torch.cuda.amp.GradScaler()
+        if precision == 'bf16':
+            autocast_dtype = torch.bfloat16
+        elif precision == 'fp16':
+            autocast_dtype = torch.float16
+        else:
+            autocast_dtype = torch.float32
         for epoch in range(epochs):
             model.train()
             optimizer.zero_grad()
-            inputs = torch.stack(self.inputDataset)
-            outputs = torch.stack(self.outputDataset)
-            predictions = model(inputs)
-            loss = criterion(predictions, outputs)
+            inputs = torch.stack(self.inputDataset).cuda()
+            outputs = torch.stack(self.outputDataset).cuda()
+            model = model.cuda()
+            with torch.cuda.amp.autocast(dtype=autocast_dtype):
+                predictions = model(inputs)
+                loss = criterion(predictions, outputs)
             normOfTrue = self.getNormOfWeights(model, self.trueParameters)
             normOfStart = self.getNormOfWeights(model, self.startParameters)
             print(f'epoch {epoch}, loss = {loss.item():.4f}, normOfTrue = {normOfTrue:.4f}, normOfStart = {normOfStart:.4f}')
             writer.add_scalar('Loss/train', loss.item(), epoch)
             writer.add_scalar('Norm/True', normOfTrue, epoch)
             writer.add_scalar('Norm/Start', normOfStart, epoch)            
+
+            # scaling backward
+            # scaler.scale(loss).backward()
+            # scaler.step(optimizer)
+            # scaler.update()
+            # optimizer.step()
+            
+            # regular backward
             loss.backward()
             optimizer.step()
--- main.py
+++ main.py
@@ -5,20 +5,34 @@ from model import Model
 from generator import Generator
 from learning import Learning
 from torch.utils.tensorboard import SummaryWriter
-writer = SummaryWriter()
+# writer = SummaryWriter()
 
 
 if __name__ == '__main__':
+    print("learning for fp32")
     model = Model(inputSize=3, outputSize=10, hiddenSize=10)
     criterion = nn.MSELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
     generator = Generator(100, 'normal', (0, 1))
     generator.generateDataset(model)
+    generator.saveParameters(model)
+    generator.createNewParameters(model)
+    
+    writer = SummaryWriter(log_dir='/home/adanilishin/Quantization/logs/fp32')
+    learner = Learning(generator.trueParameters, generator.startParameters, model, generator.inputDataset, generator.outputDataset, optimizer, criterion)
+    learner.train(model, criterion, optimizer, writer, epochs=1000, precision='fp32')
+    writer.close()
 
+    print("learning for bf16")
+    model = Model(inputSize=3, outputSize=10, hiddenSize=10)
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+    generator = Generator(100, 'normal', (0, 1))
+    generator.generateDataset(model)
     generator.saveParameters(model)
     generator.createNewParameters(model)
 
+    writer = SummaryWriter(log_dir='/home/adanilishin/Quantization/logs/bf16')
     learner = Learning(generator.trueParameters, generator.startParameters, model, generator.inputDataset, generator.outputDataset, optimizer, criterion)
-    learner.train(model, criterion, optimizer, writer, epochs=1000)
+    learner.train(model, criterion, optimizer, writer, epochs=1000, precision='bf16')
     writer.close()
-
